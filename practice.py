185b4bf975045e77159d0f58672c2bed6b6bbb65
Ntwadumela42 <35854422+Ntwadumela42@users.noreply.github.com>
2018-04-12 13:38:37 -0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -46,3 +46,13 @@ def find_short(s):
 
 def find_short(s):
     return min(len(x) for x in s.split())
+
+
+"""title_case('a clash of KINGS', 'a an the of') # should return: 'A Clash of Kings'
+title_case('THE WIND IN THE WILLOWS', 'The In') # should return: 'The Wind in the Willows'
+title_case('the quick brown fox') # should return: 'The Quick Brown Fox'"""
+
+# def title_case(title, minor_words=''):
+#     title = title.capitalize().split()
+#     minor_words = minor_words.lower().split()
+#     return ' '.join([word if word in minor_words else word.capitalize() for word in title])
